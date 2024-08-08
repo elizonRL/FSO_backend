914bba813b30b046b15f0659b0fb6923c5b1afe0
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2024-08-08 11:04:38 -0400

    FSO_backEnd first part

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+let Persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+const generateId = () => {
+  const randomId = Math.floor(Math.random() * 1000) + 1;
+  return randomId;
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(Persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = Persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  Persons = Persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+  Persons = Persons.concat(person);
+  res.json(person);
+});
+
+app.get("/info", (req, res) => {
+  const date = new Date();
+  res.send(
+    `Phonebook has info for ${
+      Persons.length
+    } people <br> ${date} <br> Id Random ${generateId()}`
+  );
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
