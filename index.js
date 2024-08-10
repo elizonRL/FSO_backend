2e603562a8f27c3fdf8f0947e821daa07294b6b8
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2024-08-09 22:50:17 -0400

    FSO back first part

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 
 const app = express();
@@ -8,6 +9,8 @@ morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+app.use(cors());
+app.use(express.static("dist"));
 
 let Persons = [
   {
@@ -97,7 +100,7 @@ app.get("/info", (req, res) => {
   );
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
