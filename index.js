038d1d2ac9a25b76e1d01041646061c0f6bc4cc6
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2024-08-18 10:27:00 -0400

    FSO Part 3 exc 3.19 to 3.21

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.delete("/api/persons/:id", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
 
   if (!body.name || !body.number) {
@@ -56,9 +56,12 @@ app.post("/api/persons", (req, res) => {
     name: body.name,
     number: body.number,
   });
-  person.save().then((savedPerson) => {
-    res.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 app.put("/api/persons/:id", (req, res, next) => {
   const { name, number } = req.body;
@@ -93,6 +96,9 @@ const errorHandler = (error, req, res, next) => {
   console.error(error.message);
   if (error.name === "CastError") {
     return res.status(400).send({ error: "malformatted id" });
+  }
+  if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
   }
 
   next(error);
--- models/dataBase.js
+++ models/dataBase.js
@@ -7,8 +7,19 @@ mongoose.set("strictQuery", false);
 mongoose.connect(url).then(console.log("connected to MongoDB"));
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate:{
+      validator: function(v){
+        return /^\d{2,3}-\d{7,}$/.test(v);
+      }
+    }
+  },
 });
 
 personSchema.set("toJSON", {
